417ff001494cfcf11b752ca1e481f163b5172dd1
saiotest <elyermad@gmail.com>
2024-10-17 23:56:37 -0300

    feat: add conditional CI on conftest

--- conftest.py
+++ conftest.py
@@ -1,12 +1,19 @@
 # This File will be used soon in a future Stream to demonstrate how to use fixtures in Pytest
 import pytest
+import os
 from selenium.webdriver import Chrome
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options as ChromeOpt
+from dotenv import load_dotenv
+load_dotenv()
+
+ON_CI = os.getenv('CI') == 'true'
 
 @pytest.fixture
 def web():
     options = ChromeOpt()
-    options.add_argument("--headless")
+    if(ON_CI):
+        options.add_argument("--headless")
+        
     web = Chrome(options=options)
     return web
